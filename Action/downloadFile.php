0ab2eec0a5201fb9cc0ed6544ef88ced16801028
Kushang Shah <kushangshah131@gmail.com>
2020-11-03 22:10:25 +0530

    Create downloadFile.php

---
+++ Action/downloadFile.php
@@ -0,0 +1,38 @@
+<?php
+
+require_once("../Config/ftpConnect.php");
+include("../Classes/classes.php");
+session_start();
+
+if(isset($_SESSION['ftp_User']) && isset($_SESSION['login_details']) && isset($_SESSION['data'])){
+	
+	$ftpUser = $_SESSION['ftp_User'];
+	$data = $_SESSION['data'];
+	$name = $_REQUEST['name'];
+	$local_file = "C:\\Users\\kushang\\Downloads\\".$name;
+	
+	if($ftpUser->checkLogin()){
+	//user has logged in area
+
+		$name = $_REQUEST['name'];
+		
+		if($ftpUser->downloadFile($data, $ftp_conn, $local_file, $name)){
+			
+		}
+		else{
+			echo "Something went wrong";
+		}
+
+	}
+	else{
+		
+	//user has not logged in
+		header("Location:../logout.php");
+		
+	}
+}
+else{
+	header("Location:../logout.php");
+}
+require_once("../Config/ftpClose.php");
+?>
