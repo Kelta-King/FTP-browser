fff3f443c2b43bb2ccf54608826451625e510dc6
Kushang Shah <kushangshah131@gmail.com>
2020-11-04 16:51:12 +0530

    Create updatePath.php

---
+++ Load/updatePath.php
@@ -0,0 +1,34 @@
+<?php
+
+require_once("../Config/ftpConnect.php");
+include("../Classes/classes.php");
+session_start();
+
+if(isset($_SESSION['ftp_User']) && isset($_SESSION['login_details']) && isset($_SESSION['data'])){
+	
+	$ftpUser = $_SESSION['ftp_User'];
+	$data = $_SESSION['data'];
+	
+	if($ftpUser->checkLogin()){
+	//user has logged in area
+
+		if($ftpUser->updatePath($data, $ftp_conn)){
+			
+		}
+		else{
+			echo "Something went wrong";
+		}
+
+	}
+	else{
+		
+	//user has not logged in
+		header("Location:../logout.php");
+		
+	}
+}
+else{
+	header("Location:../logout.php");
+}
+require_once("../Config/ftpClose.php");
+?>
